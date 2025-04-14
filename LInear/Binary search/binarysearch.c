e4463812dbef55970c6e9f87ede13fb4ebb612fe
MD.ABU NAYEM <rajnayem882@gmail.com>
2025-04-14 23:14:28 +0600

    first commit

--- LInear/Binary search/binarysearch.c
+++ LInear/Binary search/binarysearch.c
@@ -4,7 +4,7 @@ int binarysearch(int arr[] , int n , int target){
     int min = 0 ;
     int max=n-1;
     while(min <= max){
-        int mid=min + (max-min)/2;
+        int mid=(min+max)/2;
 
         if(arr[mid]==target){
             return mid;
@@ -25,9 +25,8 @@ printf("Enter the array size : ");
 scanf("%d",&n);
 int arr[n];
 
-printf("Enter the array element in ascending order: \n");
+printf("Enter the %d array element in ascending order: \n",n);
 for(int i=0 ; i<n ; i++){
-        printf("Enter element at arr[%d] index || %d position:",i,i+1);
         scanf("%d",&arr[i]);
     }
 
@@ -39,7 +38,6 @@ for(int i=0 ; i<n ; i++){
     printf("\n");
     printf("Enter the element you want to search :");
     scanf("%d",&target);
-
     result=binarysearch(arr , n ,target );
 
         if (result != -1) {
